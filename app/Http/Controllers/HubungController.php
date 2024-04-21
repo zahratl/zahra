b98c239d8cecc33e8a9259a0ed2fe6a15aa0b565
unknown <you@example.com>
2024-04-21 15:59:53 +0700

    first commit

---
+++ app/Http/Controllers/HubungController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HubungController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $a = $request->angka1;
+        $b = $request ->angka2;
+        $hasil = $a + $b;
+
+        $op = $request->op;
+
+        if($op=="+"){
+            $hasil = $a + $b;
+        }elseif($op=="-"){
+            $hasil + $a - $b;
+        }elseif($op=="x"){
+            $hasil + $a * $b;
+        }elseif($op=="/"){
+            $hasil = $a / $b;
+
+        }else{
+            $hasil="";
+            return view('kalkulator.kalkulator2', compact('hasil'));
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/kalkulator/hasil.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center><h1>KALULATOR SEDERHANA</h1></center>
+
+    <form action="{{ route(hitung.store) }}" method="post">
+        @csrf
+        Angka Pertama : <input type="number" name="angka1" id="">
+        Angka Kedua : <input type="number" name="angka2" id="">
+
+        <input type="submit" name="op" value="+">
+        <input type="submit" name="op" value="-">
+        <input type="submit" name="op" value="x">
+        <input type="submit" name="op" value="/">
+        <input type="submit" name="op" value="Reset">
+    </form>
+
+   <h1><center>Hasil : {{ $hasil }}</center></h1>
+</body>
+</html>
